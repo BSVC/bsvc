7ed3cd8e8d3e92801d8283d8244efa2f4f6f0124
Dan Cross <crossd@gmail.com>
2016-06-29 16:51:53 -0400

    Start using std::unique_ptr in the simulator.
    Memory errors due to misuse of new/delete or malloc/free
    can be vexing.  Start making use of C++11 smart pointers.
    When we can safely move to C++14, we can use 'make_unique'
    which will be rather nicer.
    
    Signed-off-by: Dan Cross <crossd@gmail.com>

--- src/M68k/sim68360/main.cpp
+++ src/M68k/sim68360/main.cpp
@@ -1,21 +1,19 @@
 // Instantiates all of the objects and starts the user interface command parser.
 
+#include <memory>
+
 #include "Framework/Interface.hpp"
 #include "M68k/sim68360/cpu32.hpp"
 #include "M68k/devices/DeviceRegistry.hpp"
 #include "M68k/loader/Loader.hpp"
 
 int main() {
-  BasicCPU &processor = *(new cpu32);
-  BasicLoader &loader = *(new Loader(processor));
-  BasicDeviceRegistry &registry = *(new DeviceRegistry);
+  auto processor = std::unique_ptr<BasicCPU>(new cpu32);
+  auto loader = std::unique_ptr<BasicLoader>(new Loader(*processor));
+  auto registry = std::unique_ptr<BasicDeviceRegistry>(new DeviceRegistry);
 
-  Interface interface(processor, registry, loader);
+  Interface interface(*processor, *registry, *loader);
   interface.CommandLoop();
-
-  delete &registry;
-  delete &loader;
-  delete &processor;
 
   return 0;
 }
