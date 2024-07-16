b0941573fdcbe666b6f2f93ce3b0b111e2e65e2f
Mikael Strom <mikael.strom@shortlink.se>
2024-07-17 06:40:53 +0800

    Updated to Python 3. Updated to work with GNU Binutils 2.42 (latest as of 2024). Added support for asm sources. I used MIR format for testing, but should work with native GNU AS format. Moved line numbers column so bsvc can use the list files without patching.

--- misc/sim68360/gal.py
+++ misc/sim68360/gal.py
@@ -12,16 +12,19 @@
 #
 # This version addresses the 'binutils-2.7' new (?) map format.
 #
+# Updatded for Python 3 and GNU GNU Binutils 2.42 (latest as of 2024).
+#   This version was tested with several assembler and C sources linked.
+#
 # * USUAL DISCLAIMER... NO WARRANTY FOR ANY PURPOSE. *
 #
+
 
 import sys
 import string
 import regex
 
 #-----------------------------------------------------------------
-debug=10
-
+debug=0
 #-----------------------------------------------------------------
 class c_function:
 	def __init__(self, name, address):
@@ -33,45 +36,45 @@ class c_variable:
 	def __init__(self, name, address):
 		self.name=name
 		self.address=address
-				
+
 #-----------------------------------------------------------------
 class c_map:
 	def __init__(self):
 		self.functions=[]
 		self.variables=[]
-	   
+
 	def load(self, file_name):
 		global debug
 		if(debug>=1):
-			print "Processing %s" % file_name
+			print ("Processing %s" % file_name)
 		try:
 			mfile=open(file_name,"r")
 		except IOError:
-			print "ERROR: [map] Can't open file %s for reading" % file_name
+			print ("ERROR: [map] Can't open file %s for reading" % file_name)
 			return 1
-		
+
 		state=0
 		while(1):
 			try:
 				s=mfile.readline()
 			except IOError:
-				print "ERROR: [map] cannot read file %s" % file_name
+				print ("ERROR: [map] cannot read file %s" % file_name)
 			if(s==""):
 				break
 			if(s=="**LINK EDITOR MEMORY MAP**\n"):
 				state=1
 				if(debug>=16):
-					print "map: link editor memory map found"
+					print ("map: link editor memory map found")
 				break
 			# new version
 			if(s=="Linker script and memory map\n"):
 				state=2
 				if(debug>=16):
-					print "map: link editor memory map found"
+					print ("map: link editor memory map found")
 				break
 
 		if(state==0):
-			print "ERROR: [map] link editor memory map not found"
+			print ("ERROR: [map] link editor memory map not found")
 			try:
 				mfile.close()
 			except IOError:
@@ -83,90 +86,90 @@ class c_map:
 				try:
 					s=mfile.readline()
 				except IOError:
-					print "ERROR: [map] cannot read file %s" % file_name
+					print ("ERROR: [map] cannot read file %s" % file_name)
 				if(s==""):
 					break
 				if(s=="\n"):
 					# ignore blank lines
 					continue
-			
+
 				i1=string.find(s,"from")
 				if(i1>=0):
 					x=string.split(s)
 					if(len(x)<2):
-						print "Warning: bad \"from\" instruction (1)" 
+						print ("Warning: bad \"from\" instruction (1)" )
 						continue
 					i2=string.find(x[1],"(")
 					if(i2==-1):
-						print "Warning: bad \"from\" instruction (2)"
-						continue		
+						print ("Warning: bad \"from\" instruction (2)")
+						continue
 					module=x[1][0:i2]
 					if(debug>=16):
-						print "Module =", module
+						print ("Module =", module)
 					i3=string.find(x[1],")")
 					if(i3==-1):
-						print "Warning: bad \"from\" instruction (3)"
-						continue		
+						print ("Warning: bad \"from\" instruction (3)")
+						continue
 					segment=x[1][i2+1:i3]
 					if(debug>=16):
-						print "Segment =", segment
+						print ("Segment =", segment)
 					continue
-									
+
 				i1=string.find(s,"output")
 				if(i1==0):
 					if(debug>=16):
-						print "\"output\" line ignored" 
+						print ("\"output\" line ignored" )
 					continue
-	
+
 				i1=string.find(s,"section")
 				if(i1==0):
 					if(debug>=16):
-						print "\"section\" line ignored" 
+						print ("\"section\" line ignored" )
 					continue
-	
+
 				i1=string.find(s,".data")
 				if(i1>=0):
 					if(debug>=16):
-						print "\".data\" line ignored" 
+						print ("\".data\" line ignored" )
 					continue
-	
+
 				i1=string.find(s,".text")
 				if(i1>=0):
 					if(debug>=16):
-						print "\".text\" line ignored" 
+						print ("\".text\" line ignored" )
 					continue
-	
+
 				i1=string.find(s,".bss")
 				if(i1>=0):
 					if(debug>=16):
-						print "\".bss\" line ignored" 
+						print ("\".bss\" line ignored" )
 					continue
-	
+
 				i1=string.find(s,"LOAD")
 				if(i1>=0):
 					if(debug>=16):
-						print "\"LOAD\" line ignored"
+						print ("\"LOAD\" line ignored")
 					continue
-							
+
 				x=string.split(s)
 				if(len(x)!=2):
 					if(debug>=6):
-						print "line ignored: \"%s\"" % s[0:-1]
+						print ("line ignored: \"%s\"" % s[0:-1])
 					continue
-				
+
 				try:
 					address=string.atoi(x[0],16)
 				except ValueError:
 					if(debug>=6):
-						print "not an address \"%s\", line ignored" % x[1]
+						print ("not an address \"%s\", line ignored" % x[1])
 					continue
-						
+
 				name=x[1]
 				##print "Name=%s, Address=%#x" % (name,address)
 				if(segment==".text"):
 					f=c_function(name,address)
 					self.functions.append(f)
-				elif (segment==".data"):	
+				elif (segment==".data"):
 					v=c_variable(name,address)
 					self.variables.append(v)
 
@@ -176,50 +179,47 @@ class c_map:
 				try:
 					s=mfile.readline()
 				except IOError:
-					print "ERROR: [map] cannot read file %s" % file_name
-				##print "READ:", s,	
+					print ("ERROR: [map] cannot read file %s" % file_name)
+				##print "READ:", s,
 				if(s==""):
 					break
 				if(s=="\n"):
 					# ignore blank lines
 					continue
 
-				i1=string.find(s,"LOAD")
+				i1=s.find("LOAD")
 				if(i1>=0):
 					continue
-				i1=string.find(s,"OUTPUT")
+				i1=s.find("OUTPUT")
 				if(i1>=0):
 					continue
-				i1=string.find(s,"COMMON")
+				i1=s.find("COMMON")
 				if(i1>=0):
 					continue
-				
-				
-				z=string.split(s)
-				
-				if((z[0]==".data") and (len(z)>=4)):
-					current_segment=1
-					##print "DATA FOR MODULE", z[3]
+				i1=s.find(".comment")
+				if(i1>=0):
 					continue
 
-				if((z[0]==".bss") and (len(z)>=4)):
-					current_segment=1
-					##print "BSS FOR MODULE", z[3]
-					continue
+				z=s.split()
 
-				if((z[0]==".text") and (len(z)>=4)):
-					current_segment=2
-					##print "TEXT FOR MODULE", z[3]
-					continue
-				
+				if(z[0][0]=='.'):
+					if(((z[0]==".data") or (z[0]==".bss")) and (len(z)>=4)):
+						current_segment=1
+						##print "DATA FOR MODULE", z[3]
+						continue
+					elif (len(z) >= 4):
+						current_segment=2
+						##print "TEXT FOR MODULE", z[3]
+						continue
+
 				if((len(z)==2) and (current_segment!=0)):
 					try:
-						address=string.atoi(z[0], 16)
+						address=int(z[0], 16)
 					except ValueError:
 						if(debug>=6):
-							print "not an address \"%s\", line ignored" % z[1]
+							print ("not an address \"%s\", line ignored" % z[1])
 						continue
-					
+
 					if(current_segment==2):
 						f=c_function(z[1],address)
 						self.functions.append(f)
@@ -228,13 +228,12 @@ class c_map:
 						v=c_variable(z[1],address)
 						self.variables.append(v)
 						##print "VARIABLE:", z[1], "at", address
-							
-									
+
 		try:
 			mfile.close()
 		except IOError:
 			pass
-		
+
 		return 0
 
 	def find_function(self, n):
@@ -244,7 +243,7 @@ class c_map:
 				f=i
 				break
 		return f
-			
+
 	def find_variable(self, n):
 		v=None
 		for i in self.variables:
@@ -252,165 +251,291 @@ class c_map:
 				v=i
 				break
 		return v
-			
+
+
 #-----------------------------------------------------------------
-def process_listing(map, ofile, ifile, ifn):
+
+def columns(file):
+	# determine columns. we look for somethig like
+	#   18 0008 4EB9 0000		jsr sub
+	#0     6    11   16  20
+
+	hex_sym = set("0123456789ABCDEF")
+
+	while True:
+		s = file.readline()
+		if not s:
+			print("Can't determine columns")
+			return [6, 11, 16, 20]
+
+		if (regex.match(r"[ ]+([0-9]+)[ ]([0-9a-fA-F]+)[ ]([0-9a-fA-F]+)[ ]([0-9a-fA-F]+)(.*)",s)):
+			i = 0;
+			while s[i].isspace():
+				i += 1
+			while s[i].isnumeric():
+				i += 1
+			while s[i].isspace():
+				i += 1
+			addr = i + 1;
+
+			while s[i].upper() in hex_sym:
+				i += 1
+			while s[i].isspace():
+				i += 1
+			data1 = i + 1
+
+			while s[i].upper() in hex_sym:
+				i += 1
+			while s[i].isspace():
+				i += 1
+			data2 = i + 1
+
+			while s[i].upper() in hex_sym:
+				i += 1
+			rest = i + 1
+
+			return [addr, data1, data2, rest]
+
+#-----------------------------------------------------------------
+def write_lst(ofile, s, address, cols):
+	x = s.split()
+	if len(x) < 1:
+		return
+	ln = x[0]
+
+	if address >= 0:
+		ns = "%0*x" % (cols[1]-cols[0]-1, address)
+	else:
+		ns = ' ' * (cols[0]-2)
+
+	ns += ' '
+
+	for i in range(cols[1]-1, cols[3]-1):
+		if i < len(s) and not s[i] in ['\r', '\n']:
+			ns += s[i]
+		else:
+			ns += ' '
+
+	ns += "%*s" % (cols[1]-cols[0]-1, ln)
+
+	if len(s) >= cols[3]-1:
+		ns += s[cols[3]-1:]
+	else:
+		ns += '\n'
+
+	ofile.write(ns)
+
+#-----------------------------------------------------------------
+
+def process_listing(map, ofile, ifile, ifn, cols):
 	global debug
-	
-	print "Processing %s" % ifn
+
+	c_src = False
+	s = ""
+
+	print ("Processing %s" % ifn)
 	state=0 #state says if addresses must be converted or not
 	while(1):
-		s=ifile.readline()
+		last = s
+		s = ifile.readline()
+
+		if not s:
+			return
+
+		x = s.split()
+		if len(x) > 0 and not x[0].isnumeric():
+			continue	# line number first on source lines
+
+		i1=s.find(".file")
+		if(i1 >= 0):
+			try:
+				name = s.split()[2]
+				ext = name[name.rfind('.')+1:-1].lower()
+				if (ext in ['c', 'cpp', 'cc', 'ccx', 'h', 'hpp']):
+					c_src = True;
+			except:
+				print("Bogus file name.")
+			write_lst(ofile, s, -1, cols)
+			continue
 
 		if(s==""):
-			break
-		i1=string.find(s,"****")
+			continue
+		i1=s.find("****")
 		if(i1>=0):
 			# c source (most likely!?)
-			ofile.write(s)
+			write_lst(ofile, s, -1, cols)
 			continue
-		i1=string.find(s,".stab")
+		i1=s.find(".stab")
 		if(i1>=0):
 			# c stabs - ignore them
-			###ofile.write(s)
+			write_lst(ofile, s, -1, cols)
 			continue
-		i1=string.find(s,"68K GAS")
+		i1=s.find("68K GAS")
 		if(i1>=0):
 			# header of listing (page number) - ignore them
-			###ofile.write(s)
 			continue
-		i1=string.find(s,"DEFINED SYMBOLS")
-		i2=string.find(s,"UNDEFINED SYMBOLS")
+		i1=s.find("DEFINED SYMBOLS")
+		i2=s.find("UNDEFINED SYMBOLS")
 		if((i1>=0)or(i2>=0)):
 			# at the end of the listing there are 2 lists: defined
 			# and undefined symbols...stop there! (not any more
 			# interesting stuff)
-			###ofile.write(s)
 			break
-		
+
 		# .globl line is "{line number} .globl {symbol name}"
-		i1=string.find(s,".globl")
+		i1=s.find(".globl")
 		if(i1==-1):
-			i1=string.find(s,".global")
+			i1=s.find(".global")
 		if(i1>=0):
 			state=0
-			x=string.split(s)
+			x=s.split()
 			if(len(x)<2):
 				if(debug>=5):
-					print "Warning: malformed .globl", x,
-				ofile.write(s)
+					print ("Warning: malformed .globl", x,)
+				write_lst(ofile, s, -1, cols)
 				continue
-			
+
 			if(debug>=6):
-				print "\tanalysing %s, line %s" % (x[2],x[0])
+				print ("\tanalysing %s, line %s" % (x[2],x[0]))
 			f=map.find_function(x[2])
 			if(f!=None):
 				if(debug>=15):
-					print "Function %s at %#x found" % (f.name,f.address)
+					print ("Function %s at %#x found" % (f.name,f.address))
 				state=1
 				delta=0
 				###ofile.write("* FUNCTION %s at %#04x\n" % (f.name,f.address))
-		 	v=map.find_variable(x[2])
+			v=map.find_variable(x[2])
 			if(v!=None):
 				if(debug>=15):
-					print "Variable %s at %#x found" % (v.name,v.address)
+					print ("Variable %s at %#x found" % (v.name,v.address))
 				state=3
-				delta=0	
+				delta=0
 				###ofile.write("* VARIABLE %s at %#04x\n" % (v.name,v.address))
 			if((f==None)and(v==None)and(debug>=1)):
-				print "Warning: global %s not in map!" % x[2]
-										
+				print ("Warning: global %s not in map!" % x[2])
+
+		if (not c_src):
+			# if last line was a label and this line is recoginzed, then assembler source
+			s0 = last.split();
+			if (len(s0) >= 2 and s0[0].isnumeric()):
+				if (s0[1][-1] == ':'):
+					sym = s0[1][:-1]
+				else:
+					sym = s0[0]
+				tmp = map.find_function(sym)
+				if (tmp == None):
+					tmp = map.find_variable(sym)
+				if (tmp != None):
+					result = regex.match(r"[ ]+([0-9]+)[ ]([0-9a-fA-F]+)[ ]([0-9a-fA-F]+)(.*)",s)
+					if (result):
+						f=tmp;
+						state = 4
+						x=s.split()
+						delta=int(x[1],16)
+
 		if(state==1):
-			# the first time, inside a function			
-			result=regex.match("[ ]+\([0-9]+\) \([0-9a-fA-F]+\) \([0-9a-fA-F]+\)\(.*\)",s)
-			if(result>=0):
+			# the first time, inside a function
+			result=regex.match(r"[ ]+([0-9]+) ([0-9a-fA-F]+) ([0-9a-fA-F]+)(.*)",s)
+			if(result):
 				###print "MATCH", s,
-				x=string.split(s)
+				x=s.split()
 				# old adress is x[1]
 				try:
-					delta=string.atoi(x[1],16)
+					delta=int(x[1],16)
 				except ValueError:
-					print "ERROR: Bad address %s line %s" % (x[1],x[0])
+					print ("ERROR: Bad address %s line %s" % (x[1],x[0]))
 					break
-				new_address=hex(f.address)
-				ofile.write("%s %s " % (x[0],new_address[2:]))
-			   	for e in x[2:]:
-					ofile.write(e+" ")				
-				ofile.write("\n")
+				write_lst(ofile, s, f.address, cols)
 				state=2
 			else:
-				ofile.write(s)
-				
+				write_lst(ofile, s, -1, cols)
+
 		elif(state==2):
 			# inside a function, when delta is known
-			result=regex.match("[ ]+\([0-9]+\)[ ]+\([0-9a-fA-F]+\)[ ]+\([0-9a-fA-F]+\)[ ]+\(.*\)",s)
-			if(result>=0):
+			result=regex.match(r"[ ]+([0-9]+)[ ]+([0-9a-fA-F]+)[ ]+([0-9a-fA-F]+)[ ]+(.*)",s)
+			if(result):
 				###print "MATCH", s,
-				x=string.split(s)
+				x=s.split()
 				# old adress is x[1]
 				try:
-					new_address=hex(string.atoi(x[1],16)-delta+f.address)
+					new_address=int(x[1],16)-delta+f.address
 				except ValueError:
-					print "ERROR: Bad address %s line %s" % (x[1],x[0])
+					print ("ERROR: Bad address %s line %s" % (x[1],x[0]))
 					break
-				ofile.write("%s %s " % (x[0],new_address[2:]))
-			   	for e in x[2:]:
-					ofile.write(e+" ")				
-				ofile.write("\n")
+				write_lst(ofile, s, new_address, cols)
 			else:
 				###print "DONT MATCH", s,
-				ofile.write(s)			
-						
+				write_lst(ofile, s, -1, cols)
+
 		elif (state==3):
 			# variable definition
-			result=regex.match("[ ]+\([0-9]+\) \([0-9a-fA-F]+\) \([0-9a-fA-F]+\)\(.*\)",s)
-			if(result>=0):
+			result=regex.match(r"[ ]+([0-9]+) ([0-9a-fA-F]+) ([0-9a-fA-F]+)(.*)",s)
+			if(result):
 				###print "MATCH", s,
-				x=string.split(s)
-				# old adress is x[1]
-				new_address=hex(v.address)
-				ofile.write("%s %s " % (x[0],new_address[2:]))
-			   	for e in x[2:]:
-					ofile.write(e+" ")				
-				ofile.write("\n")
-				#a var. is defined in 1 line: as soon as there
-				# is a match, return to state 0
-				state=0 
+				x=s.split()
+				write_lst(ofile, s, v.address, cols)
+				state=0
 			else:
-				ofile.write(s)
+				write_lst(ofile, s, -1, cols)
+
+		elif(state == 4):
+			# in assembler file
+			result = regex.match(r"[ ]+([0-9]+)[ ]([0-9a-fA-F]+)[ ]([0-9a-fA-F]+)(.*)", s)
+			if(result):
+				x = s.split()
+				try:
+					new_address = int(x[1], 16) - delta + f.address
+				except ValueError:
+					print ("ERROR: Bad address %s line %s" % (x[1], x[0]))
+					break
+				write_lst(ofile, s, new_address, cols)
+			else:
+				###print "DONT MATCH", s,
+				write_lst(ofile, s, -1, cols)
+
 		else:
-			###print "????", s,
-			ofile.write(s)
-	
-	
+			write_lst(ofile, s, -1, cols)
+
+
 #-----------------------------------------------------------------
 def main(argv):
 	if(len(argv)<2):
-		print "usage %s map-file listing-files" % argv[0]
+		print ("usage %s map-file listing-files" % argv[0])
 		sys.exit(-1)
 
 	map=c_map()
 	if(map.load(argv[1])):
 		sys.exit(1)
-		
-	index=string.rfind(argv[1],'.')
+
+	if(debug>=20):
+		for x in map.functions:
+			print("Function:", x.name, hex(x.address))
+		for x in map.variables:
+			print("Variable: ", x.name, hex(x.address))
+
+	index=argv[1].rfind('.')
 	n=argv[1][0:index]+".LIS"
-		
+
 	try:
 		ofile=open(n,"w")
 	except IOError:
-		print "ERROR: Can't open file %s for writing" % n
+		print ("ERROR: Can't open file %s for writing" % n)
 		sys.exit(2)
-		
+
 	for lf in argv[2:]:
 		try:
 			ifile=open(lf,"r")
 		except IOError:
-			print "ERROR: Can't open file %s for reading" % lf
+			print ("ERROR: Can't open file %s for reading" % lf)
 			ofile.close()
 			sys.exit(2)
 
-		process_listing(map,ofile,ifile,lf)
+		cols = columns(ifile)
+		ifile.seek(0,0)
+
+		ofile.write("\n*** " + lf + " ***\n\n")
+
+		process_listing(map,ofile,ifile,lf, cols)
 
 		try:
 			ifile.close()
@@ -421,7 +546,7 @@ def main(argv):
 		ofile.close()
 	except IOError:
 		pass
-			
+
 #-----------------------------------------------------------------
 main(sys.argv)
 #-----------------------------------------------------------------
